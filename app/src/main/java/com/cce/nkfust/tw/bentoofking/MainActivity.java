f5739ae77f70fb3a86fe3d60d791c97dca71bd36
87 <bleakbear15@gmail.com>
2018-03-03 22:42:14 +0800

    def

--- app/src/main/java/com/cce/nkfust/tw/bentoofking/MainActivity.java
+++ app/src/main/java/com/cce/nkfust/tw/bentoofking/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         int abc;
+        int def;
         setContentView(R.layout.activity_main);
     }
 }
