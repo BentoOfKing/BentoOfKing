217f3b77333df87984030ee1c92a1feb984b5a59
John <john8654john@gmail.com>
2018-03-06 00:02:42 +0800

    toolbar 2018/3/6

--- app/src/main/java/com/cce/nkfust/tw/bentoofking/MainActivity.java
+++ app/src/main/java/com/cce/nkfust/tw/bentoofking/MainActivity.java
@@ -2,14 +2,19 @@ package com.cce.nkfust.tw.bentoofking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         int abc;
         int def;
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("便當王");
+        toolbar.setNavigationIcon(R.drawable.ic_menu_black_24dp);
     }
 }
