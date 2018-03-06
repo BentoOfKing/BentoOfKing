28a69542d292b5fcf4efaf60d1e220720586412d
John <john8654john@gmail.com>
2018-03-07 00:12:38 +0800

    DrawerLayout 2018/3/7

--- app/src/main/java/com/cce/nkfust/tw/bentoofking/MainActivity.java
+++ app/src/main/java/com/cce/nkfust/tw/bentoofking/MainActivity.java
@@ -1,20 +1,31 @@
 package com.cce.nkfust.tw.bentoofking;
 
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private ListView drawerListView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        int abc;
-        int def;
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.toolbar_menu);
         toolbar.setTitle("便當王");
-        toolbar.setNavigationIcon(R.drawable.ic_menu_black_24dp);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawerListView = (ListView) findViewById(R.id.drawerListView);
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerToggle.syncState();
+        drawerToggle.getDrawerArrowDrawable().setColor(ContextCompat.getColor(this,R.color.white));
+        drawerLayout.addDrawerListener(drawerToggle);
+
     }
 }
