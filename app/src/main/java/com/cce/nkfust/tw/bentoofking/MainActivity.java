0470286ad46f6c048380471242739afb46c0ee82
John <john8654john@gmail.com>
2018-04-06 22:11:06 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/lookmember.xml
    #	app/src/main/res/values/dimens.xml

--- app/src/main/java/com/cce/nkfust/tw/bentoofking/MainActivity.java
+++ app/src/main/java/com/cce/nkfust/tw/bentoofking/MainActivity.java
@@ -2,6 +2,7 @@ package com.cce.nkfust.tw.bentoofking;
 
 import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
 
