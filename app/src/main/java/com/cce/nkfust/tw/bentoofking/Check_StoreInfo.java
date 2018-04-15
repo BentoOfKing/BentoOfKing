0f54464c459bb57e9362acc61c57aa4b105cf420
Bird <birdmandayum0131@gmail.com>
2018-04-15 12:13:59 +0800

    bird

---
+++ app/src/main/java/com/cce/nkfust/tw/bentoofking/Check_StoreInfo.java
@@ -0,0 +1,13 @@
+package com.cce.nkfust.tw.bentoofking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Check_StoreInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_02_check_storeinfo);
+    }
+}
