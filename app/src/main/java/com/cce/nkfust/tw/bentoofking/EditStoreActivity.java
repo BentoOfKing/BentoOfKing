57d1690814d02203355e52ce38f3e575c30461a6
John <john8654john@gmail.com>
2018-04-29 23:52:55 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/cce/nkfust/tw/bentoofking/MainActivity.java

---
+++ app/src/main/java/com/cce/nkfust/tw/bentoofking/EditStoreActivity.java
@@ -0,0 +1,13 @@
+package com.cce.nkfust.tw.bentoofking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditStoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_store);
+    }
+}
