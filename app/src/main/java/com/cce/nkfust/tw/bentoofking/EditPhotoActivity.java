c0d1978d6d95aff4ef8e53561bdb538ce3c7e6e9
John <john8654john@gmail.com>
2018-04-29 17:04:01 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/cce/nkfust/tw/bentoofking/MainActivity.java

---
+++ app/src/main/java/com/cce/nkfust/tw/bentoofking/EditPhotoActivity.java
@@ -0,0 +1,42 @@
+package com.cce.nkfust.tw.bentoofking;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class EditPhotoActivity extends AppCompatActivity {
+    private static String passUserInfo = "USER_INFO";
+    private static String passStoreInfo = "STORE_INFO";
+    private static String passmenuInfo = "MENU_INFO";
+    private UserInfo userInfo;
+    private Toolbar toolbar;
+    private ListView drawerListView;
+    private DrawerLayout drawerLayout;
+    private Store store;
+    private ArrayList<Meal> meal;
+    private Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_photo);
+        context = this;
+        Intent intent = getIntent();
+        userInfo = (UserInfo) intent.getSerializableExtra(passUserInfo);
+        store =(Store) intent.getSerializableExtra(passStoreInfo);
+        meal = (ArrayList<Meal>) intent.getSerializableExtra(passmenuInfo);
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        drawerListView = findViewById(R.id.drawerListView);
+        Drawer drawer = new Drawer();
+        drawer.init(this,toolbar,drawerListView,drawerLayout,userInfo);
+        drawer.setToolbarNavigation();
+        toolbar.setTitle(getResources().getString(R.string.editPhoto));
+
+    }
+}
