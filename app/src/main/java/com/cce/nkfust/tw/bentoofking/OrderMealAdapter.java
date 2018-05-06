1b9914f214faa1fc079a0fa57c6f38232945efa3
John <john8654john@gmail.com>
2018-05-06 17:14:28 +0800

    Bird comment

--- app/src/main/java/com/cce/nkfust/tw/bentoofking/OrderMealAdapter.java
+++ app/src/main/java/com/cce/nkfust/tw/bentoofking/OrderMealAdapter.java
@@ -14,30 +14,30 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by John on 2018/5/5.
  */
 
 public class OrderMealAdapter extends BaseAdapter {
-    private ArrayList<Meal> meal;
+    private ArrayList<OrderIncludeMeal> meal;
     private Context context;
     private LayoutInflater inflater;
     private ViewHolder viewHolder;
-
 
 
     public static class ViewHolder{
         RelativeLayout mealListViewLayout;
         TextView nameTextView ;
         TextView priceTextView;
-        EditText countEditText;
+        TextView countTextView;
         ImageView reduceImageView;
         ImageView addImageView;
         Meal nowMeal;
     }
 
-    public OrderMealAdapter(LayoutInflater inflater, ArrayList<Meal> meal){
+    public OrderMealAdapter(LayoutInflater inflater, ArrayList<OrderIncludeMeal> meal){
         this.meal = meal;
         //this.context = context;
         this.inflater = inflater;
@@ -50,7 +50,7 @@ public class OrderMealAdapter extends BaseAdapter {
     }
 
     @Override
-    public Meal getItem(int i) {
+    public OrderIncludeMeal getItem(int i) {
         return meal.get(i);
     }
 
@@ -60,73 +60,25 @@ public class OrderMealAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(final int position, View view, ViewGroup viewGroup) {
+
         if(view==null){
             viewHolder = new ViewHolder();
             view = this.inflater.inflate(R.layout.order_meal_item,null);
-            viewHolder.nowMeal = getItem(i);
             viewHolder.nameTextView = view.findViewById(R.id.nameTextView);
             viewHolder.priceTextView = view.findViewById(R.id.priceTextView);
-            viewHolder.countEditText = view.findViewById(R.id.countEditText);
-            viewHolder.reduceImageView = view.findViewById(R.id.reduceImageView);
-            viewHolder.addImageView = view.findViewById(R.id.addImageView);
+            viewHolder.countTextView = view.findViewById(R.id.countTextView);
             viewHolder.mealListViewLayout = view.findViewById(R.id.border);
+
             view.setTag(viewHolder);
         }else{
             viewHolder = (ViewHolder)view.getTag();
         }
-        viewHolder.nameTextView.setText(getItem(i).getName()+"，售價 "+getItem(i).getPrice()+" 元");
+        viewHolder.countTextView.setText(meal.get(position).getCount()+" 個");
+        viewHolder.nameTextView.setText(getItem(position).getMeal().getName()+"，售價 "+getItem(position).getMeal().getPrice()+" 元");
 
-
-
-        viewHolder.reduceImageView.setOnClickListener(new reduceHandler());
-        viewHolder.addImageView.setOnClickListener(new addHandler());
-        TextWatcher countTextHandler = new TextWatcher(){
-
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(!viewHolder.countEditText.getText().toString().equals("")) {
-                    try {
-                        viewHolder.priceTextView.setText("小計 " + Integer.toString(Integer.parseInt(viewHolder.countEditText.getText().toString()) * Integer.parseInt(viewHolder.nowMeal.getPrice())) + " 元");
-                    } catch (Exception e) {
-                        viewHolder.countEditText.setText("小計 0 元");
-                    }
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        };
-        viewHolder.countEditText.addTextChangedListener(countTextHandler);
+        viewHolder.priceTextView.setText("小計 "+Integer.parseInt(getItem(position).getMeal().getPrice())*Integer.parseInt(getItem(position).getCount())+" 元");
         return view;
     }
-    class addHandler implements View.OnClickListener {
-        @Override
-        public void onClick(View view) {
-            try {
-                viewHolder.countEditText.setText(Integer.toString(Integer.parseInt(viewHolder.countEditText.getText().toString()) + 1));
-            }catch (Exception e){
-                viewHolder.countEditText.setText("1");
-            }
-        }
-    }
-    class reduceHandler implements View.OnClickListener {
-        @Override
-        public void onClick(View view) {
-            try {
-                if(Integer.parseInt(viewHolder.countEditText.getText().toString()) > 0 ) {
-                    viewHolder.countEditText.setText(Integer.toString(Integer.parseInt(viewHolder.countEditText.getText().toString()) - 1));
-                }
-            }catch (Exception e){
-                viewHolder.countEditText.setText("0");
-            }
-        }
-    }
+
 }
